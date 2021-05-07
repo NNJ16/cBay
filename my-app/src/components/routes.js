cf6567995ccac83ee28b79171354359744ce71ef
mmihiranga <madhuramihiranga6@gmail.com>
2021-05-07 13:18:14 +0530

    checkout & paymentform added

--- my-app/src/components/routes.js
+++ my-app/src/components/routes.js
@@ -7,6 +7,10 @@ import Home from "../views/Home";
 import Dashboard from "../views/Seller/Dashboard";
 import AddItemForm from "../views/Seller/AddItem";
 import UpdateItemForm from "../views/Seller/UpdateItem";
+import Checkout from '../views/Checkout';
+import AddressForm from '../views/AddressForm';
+import Review from '../views/Review';
+import PaymentForm from '../views/PaymentForm';
 
 const hist = createBrowserHistory();
 
@@ -17,6 +21,12 @@ const Routes = () => (
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/additem" component={AddItemForm} />
         <Route path="/updateitem" component={UpdateItemForm} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/addressform" component={AddressForm} />
+        <Route path="/review" component={Review} />
+        <Route path="/paymentform" component={PaymentForm} />
+
+        
         <Route exact path="/" component={Home} />
     </Router>
 );
